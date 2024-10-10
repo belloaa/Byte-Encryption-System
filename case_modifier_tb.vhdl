2d2e488a25c49143ccba86d4a5f9dc99f50fa3f8
Blueface <belloabdul8@gmail.com>
2024-10-10 16:41:06 -0600

    clarity

--- case_modifier_tb.vhdl
+++ case_modifier_tb.vhdl
@@ -14,8 +14,8 @@ architecture TEST of T_CASE_MODIFIER is
         );
     end component case_modifier;
     
-    signal MESSAGE_TB     : std_logic_vector(7 downto 0) := "01000000";
-    signal MESSAGE_REG : unsigned(7 downto 0) := "01000000";
+    signal MESSAGE_TB     : std_logic_vector(7 downto 0) := "01000001";
+    signal MESSAGE_REG : unsigned(7 downto 0) := "01000001";
     signal OUT_MESSAGE_TB : std_logic_vector(7 downto 0);
     
  begin
@@ -27,24 +27,16 @@ architecture TEST of T_CASE_MODIFIER is
         process
         begin
         -- waiting for a little so it doesn't check the validation right away
-        for i in 0 to 30 loop
+        -- checking only capital letters
+        for i in 0 to 25 loop
         wait for 5 ns;
---            wait for 5 ns;
---            assert GREEN_LED_TB = '1'
---            report "Invalid Character: "
---            severity error;
-
---            wait for 5 ns;
---            assert RED_LED_TB = '1'
---            report "Valid Character: "
---            severity error;
             
-            -- cycling through all of the values of an 8bit vector
+            -- cycling through all of the relevant of the 8bit vector
             MESSAGE_REG <= MESSAGE_REG + 1;
             MESSAGE_TB     <= std_logic_vector(MESSAGE_REG);
             
         end loop;
         wait;
 
-    end process;
+    end process;    
 end TEST;
